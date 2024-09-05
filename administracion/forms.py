aefa55ae22eb4621a6677f15797f3b6ba2569614
Anderson Tovar <andersontovar09@gmail.com>
2024-09-05 13:28:51 -0400

    form style css

--- administracion/forms.py
+++ administracion/forms.py
@@ -35,6 +35,14 @@ class AveriaForm(forms.ModelForm):
     class Meta:
         model = Averia
         fields = ['problema', 'tipo_averia', 'departamento', 'ubicacion', 'serial', 'codigo_bn']
+        labels = {
+                    'problema': 'Problema',
+                    'tipo_averia': 'Tipo de avería',
+                    'departamento': 'Departamento',
+                    'ubicacion': 'Ubicación',
+                    'serial': 'Serial',
+                    'codigo_bn': 'Código BN'
+        }
         widgets = {}
         
 class CompraForm(forms.ModelForm):
